fd4a6521dcc1c665321449f0806762612325039e
Hans Gundlach <hansg17@lakesideschool.org>
2018-01-09 23:38:46 -0800

    added time sync

--- control_system/control_system.ino
+++ control_system/control_system.ino
@@ -2,6 +2,10 @@
 #include <Bridge.h>
 #include <BlynkSimpleYun.h>
 #include <math.h>
+#include <AutoPID.h>
+#include <TimeLib.h>
+#include <WidgetRTC.h>
+
 
 
 char auth[] = "4b0acc400cb5442fa471ab6de0db41d1";
@@ -14,7 +18,29 @@ float R1 = 1000;
 float logR2, R2, T;
 float c1 = 1.009249522e-03, c2 = 2.378405444e-04, c3 = 2.019202697e-07;
 
+
 BlynkTimer timer;
+WidgetRTC rtc;
+
+// Digital clock display of the time
+void clockDisplay()
+{
+  // You can call hour(), minute(), ... at any time
+  // Please see Time library examples for details
+
+  String currentTime = String(hour()) + ":" + minute() + ":" + second();
+  String currentDate = String(day()) + " " + month() + " " + year();
+  Serial.print("Current time: ");
+  Serial.print(currentTime);
+  Serial.print(" ");
+  Serial.print(currentDate);
+  Serial.println();
+
+  // Send time to the App
+  Blynk.virtualWrite(V1, currentTime);
+  // Send date to the App
+  Blynk.virtualWrite(V2, currentDate);
+}
 
 //reads temperature ad sends it off to blynk
 void myTimerEvent(){
@@ -32,18 +58,42 @@ BLYNK_WRITE(V1)
   Serial.println(pinValue);
 }
 
+//on-off button 
+BLYNK_WRITE(V2)
+{
+  int pinValue = param.asInt(); // assigning incoming value from pin V1 to a variable
+  // You can also use:
+  // String i = param.asStr();
+  // double d = param.asDouble();
+  Serial.print("V2 Slider value is: ");
+  Serial.println(pinValue);
+}
 
 void setup() {
   // Debug console
   Serial.begin(9600);
+//date time integration 
 
+
+
+//Blynk intro
   Blynk.begin(auth);
+
+  // Begin synchronizing time
+  rtc.begin();
   // put your setup code here, to run once:
   timer.setInterval(1000L,myTimerEvent);
+  timer.setInterval(1000L,clockDisplay);
 }
 
-void loop() {
+void loop() {  
+
+//==================================================================
+//BLynk app integration
   Blynk.run();
+
+
+  
 //thermistor measurmets
  Vo = analogRead(ThermistorPin);
   R2 = R1 * (1023.0 / (float)Vo - 1.0);
@@ -58,6 +108,10 @@ void loop() {
 
   delay(500);
 timer.run();
+
+
+//temperature controls 
+//myPID.run();
 
   /*temp_real
   temp_set
